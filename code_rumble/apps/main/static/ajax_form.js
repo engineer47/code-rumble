65a77135c2a9f6d9c339ea192931d559003d3cc8
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2016-01-30 20:02:57 +0200

    changes

--- code_rumble/apps/main/static/ajax_form.js
+++ code_rumble/apps/main/static/ajax_form.js
@@ -11,3 +11,21 @@ function submit_my_jobs() {
   	$("#my_jobs_form").submit();
 
 };
+
+$(document).ready(function() {
+ 
+    $('#job_table tbody').on( 'click', 'tr', function () {
+        $(this).toggleClass('selected');
+        $("tr:even").css("background-color", "white");
+        $("tr:odd").css("background-color", "white");
+        $(this).css("background-color", "#d3d3d3");
+  
+        var Something = $(this).closest('td').children('1').text();
+        var vaaal = $(this).children().val();
+        alert(vaaal);
+    } );
+ 
+   // $('#button').click( function () {
+   //     alert( table.rows('.selected').data().length +' row(s) selected' );
+    //} );
+} );
