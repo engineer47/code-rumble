a4ca7ab7203423c1b6ecf60fa1f9706dd08a088b
Coulson <ckgathi@bhp.org.bw>
2016-01-30 13:12:43 +0200

    fix view and urls

--- code_rumble/apps/main/views/__init__.py
+++ code_rumble/apps/main/views/__init__.py
@@ -1,2 +1,4 @@
 from .shipper import Shipper
+from .home import Home
 from .user_login import user_profile, users, index, login_view, signup, logout_view, verify_account
+from .base_dashboard import BaseDashboard
--- code_rumble/apps/main/views/base_dashboard.py
+++ code_rumble/apps/main/views/base_dashboard.py
@@ -1,29 +1,23 @@
+from django.views.generic import View
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from .base_dashboard import BaseDashboard
 
 
-class Shipper(BaseDashboard):
+class BaseDashboard(View):
 
     def __init__(self):
         self.context = {}
-        self.template_name = 'shipper.html'
-        super(Shipper, self).__init__()
+        self.title = 'BW Shipping Portal'
 
     def get(self, request, *args, **kwargs):
-        notifications = 'Notifications.objects.all()'
         self.context.update({
-            'title': self.title,
-            'notifications': notifications,
         })
         return render_to_response(self.template_name, self.context, context_instance=RequestContext(request))
 
     def post(self, request, *args, **kwargs):
         self.context.update({
-            'title': self.title
         })
         return render_to_response(self.template_name, self.context, context_instance=RequestContext(request))
 
     def get_context_data(self, **kwargs):
-
-        return super(Shipper, self).get_context_data(**kwargs)
+        return super(BaseDashboard, self).get_context_data(**kwargs)
