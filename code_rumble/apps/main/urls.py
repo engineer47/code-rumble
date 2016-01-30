652c19911d724f1f272e22bb9546c3fdc7c9a94a
Coulson <ckgathi@bhp.org.bw>
2016-01-30 08:13:02 +0200

    reareage code

--- code_rumble/apps/main/urls.py
+++ code_rumble/apps/main/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
-from .views import user_profile, users, index, login_view, signup, submit, logout_view
+from code_rumble.apps.main.views.user_login import user_profile, users, index, login_view, signup, submit, logout_view
+from code_rumble.apps.main.views import Shipper
 
 urlpatterns = patterns(
     '',
@@ -13,6 +14,7 @@ urlpatterns = patterns(
     url(r'^users/(?P<username>\w{0,30})/$', users),
     url(r'^user_profile/(?P<username>\w{0,30})/$', user_profile),
     url(r'^user_profile/$', user_profile),
+    url(r'shipper^$', Shipper.as_view(), name='shipper_url'),
 )
 
 urlpatterns += patterns(
---
+++ code_rumble/apps/main/views/shipper.py
@@ -0,0 +1,27 @@
+from django.views.generic import View
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+class Shipper(View):
+
+    def __init__(self):
+        self.context = {}
+        self.template_name = 'shipper.html'
+        self.title = 'This is Home'
+
+    def get(self, request, *args, **kwargs):
+        self.context.update({
+            'title': self.title,
+        })
+        return render_to_response(self.template_name, self.context, context_instance=RequestContext(request))
+
+    def post(self, request, *args, **kwargs):
+        self.context.update({
+            'title': self.title
+        })
+        return render_to_response(self.template_name, self.context, context_instance=RequestContext(request))
+
+    def get_context_data(self, **kwargs):
+
+        return super(Shipper, self).get_context_data(**kwargs)
